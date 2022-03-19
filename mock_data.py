6ca2b9008dae5011c0e623e7b41a925d47ebf711
Phil Chaplin <pchapl1@gmail.com>
2022-03-19 11:46:56 -0700

    finished class 3

---
+++ mock_data.py
@@ -0,0 +1,52 @@
+
+catalog = [
+    {
+        "_id" : 'fadsfa',
+        "title" : 'Black Split Hem',
+        "price" : 22.99,
+        "stock" : 23,
+        "category" : 'short sleeve',
+        "image" : 'black-split-hem.jpeg'
+    },
+    {
+        "_id" : 'adsfdfde',
+        "title" : 'Cab Split Hem',
+        "price" : 25.93,
+        "stock" : 2,
+        "category" : 'short sleeve',
+        "image" : 'cabernet-split-hem.jpeg'
+    },
+    {
+        "_id" : 'kjsdhfjkhr',
+        "title" : 'White Crew Hem',
+        "price" : 32.99,
+        "stock" : 5,
+        "category" : 'long sleeve',
+        "image" : 'WHITE-CREW-SPLIT-HEM.jpeg'
+    },
+    {
+        "_id" : '747EUJDJD',
+        "title" : 'White Crew',
+        "price" : 22.99,
+        "stock" : 3,
+        "category" : 'long sleeve',
+        "image" : 'WHITE-LS-CREW-CURVE-HEM.jpeg'
+    },
+    {
+        "_id" : 'O1834YRUGUIFDSG',
+        "title" : 'Desert Split Hem',
+        "price" : 52.99,
+        "stock" : 230,
+        "category" : 'short sleeve',
+        "image" : 'desert.jpeg'
+    },
+]
+
+
+# class DataService{
+#     getCatalog(){
+#         return catalog;
+#     }
+# }
+
+# export default DataService;
--- server.py
+++ server.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template
+
+import json
+from nis import cat
+from flask import Flask, request, redirect, session, abort
+from flask.templating import render_template
+from mock_data import catalog
+
+
+
 
 app = Flask('Server')
 
@@ -10,6 +18,49 @@ def index():
 @app.route('/me')
 def about_me():
     return 'Phil Chaplin'
+
+@app.route('/api/catalog', methods = ['get'])
+def get_catalog():
+    return json.dumps(catalog)
+
+@app.route('/api/catalog', methods = ['post'])
+def save_product():
+    pass
+
+@app.route('/api/catalog/count', methods = ['get'])
+def num_products():
+    return json.dumps(len(catalog))
+
+
+@app.route('/api/catalog/total', methods = ['get'])
+def total_products():
+    total = 0
+    for x in catalog:
+        total += x['price']
+    return json.dumps(total)
+
+
+@app.route('/api/about')
+def about_me():
+    about_message = 'this is the about page'
+    return json.dumps(about_message)
+
+@app.route('/api/catalog/<id>')
+def get_by_id(id):
+    try:
+        return json.dumps([x for x in catalog if x['_id'] == id][0])
+    except Exception as e:
+        return 'id doesnt exist'
+
+@app.route('/api/catalog/most_expensive')
+def most_expensive():
+    max_price = 0
+    prod_name = ''
+    for x in catalog:
+        if x['price'] > max_price:
+            max_price = x['price']
+            prod_name = x['title']
+    return json.dumps(prod_name)
 
 
 app.run(debug=True)
